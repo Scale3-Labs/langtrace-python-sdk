e5d6e3ebed2046a8884e7687ccbf9c2f4669cdd5
Karthik Kalyanaraman <karthik@scale3labs.com>
2024-02-26 06:36:13 -0800

    fix

--- src/span_attributes.py
+++
@@ -1,8 +0,0 @@
-SpanAttributes = {
-    "SERVICE_PROVIDER": "service_provider",
-    "BASE_URL": "baseURL",
-    "API": "api",
-    "MODEL": "model",
-    "REQUEST_PROMPTS": "request.prompts",
-    "RESPONSE_RESPONSES": "response.responses",
-}
