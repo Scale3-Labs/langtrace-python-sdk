17caca102e3ac81e17a86b190fbb119e95336698
Karthik Kalyanaraman <karthik@scale3labs.com>
2024-03-05 13:18:46 -0800

    fixes

--- src/instrumentation/chroma/instrumentation.py
+++ src/instrumentation/chroma/instrumentation.py
@@ -1,3 +1,6 @@
+"""
+Instrumentation for ChromaDB
+"""
 import importlib.metadata
 from typing import Collection
 
@@ -5,11 +8,14 @@ from opentelemetry.instrumentation.instrumentor import BaseInstrumentor
 from opentelemetry.trace import get_tracer
 from wrapt import wrap_function_wrapper
 
-from instrumentation.chroma.lib.apis import APIS
+from instrumentation.chroma.apis import APIS
 from instrumentation.chroma.patch import collection_patch
 
 
 class ChromaInstrumentation(BaseInstrumentor):
+    """
+    The ChromaInstrumentation class represents the ChromaDB instrumentation
+    """
 
     def instrumentation_dependencies(self) -> Collection[str]:
         return ["chromadb >= 0.4.23"]
@@ -19,7 +25,7 @@ class ChromaInstrumentation(BaseInstrumentor):
         tracer = get_tracer(__name__, "", tracer_provider)
         version = importlib.metadata.version('chromadb')
 
-        for operation, details in APIS.items():
+        for operation, _ in APIS.items():
             wrap_function_wrapper(
                 'chromadb.api.models.Collection',
                 f'Collection.{operation.lower()}',
@@ -27,7 +33,7 @@ class ChromaInstrumentation(BaseInstrumentor):
             )
 
     def _instrument_module(self, module_name):
-        print(module_name)
+        pass
 
     def _uninstrument(self, **kwargs):
-        print(kwargs)
+        pass
