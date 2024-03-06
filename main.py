94c83e7c7b689b2d31a271675ebc9750be5f4392
Karthik Kalyanaraman <karthik@scale3labs.com>
2024-03-06 11:38:27 -0800

    fixes

--- main.py
+++
@@ -1,9 +0,0 @@
-from langtrace.trace_attributes import LangTraceSpanAttributes
-from langtrace.trace_attributes import OpenAISpanAttributes
-from langtrace.trace_attributes import Event
-
-# Instantiate the model
-attributes = LangTraceSpanAttributes.model_validate({'service.provider': 'OpenAI'})
-
-# Print the model as a JSON string
-print(attributes.model_dump_json(by_alias=True))
