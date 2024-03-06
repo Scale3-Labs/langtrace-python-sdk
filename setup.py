470dc122ae303882033f2637aa05f6982d9ac2b3
Karthik Kalyanaraman <karthik@scale3labs.com>
2024-03-06 11:33:19 -0800

    fix package

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ with open('requirements.txt') as f:
 
 setup(
     name='langtrace-python-sdk',  # Choose a unique name for PyPI
-    version='1.0.13',
+    version='1.0.0',
     author='Scale3 Labs',
     license="AGPL-3.0-or-later",
-    author_email='ali@scale3labs.com',
-    maintainer=['Karthik Kalyanaraman', 'Ali Waleed', 'Rohit Kadhe'],
+    author_email='engineering@scale3labs.com',
+    maintainer=['Ali Waleed', 'Darshit Suratwala', 'Dylan Zuber',
+                'Karthik Kalyanaraman', 'Obinna Okafor', 'Rohit Kadhe', 'Yemi Adejumobi'],
     description='Python SDK for LangTrace',
     long_description="LangTrace - Python SDK",
     long_description_content_type='text/markdown',
@@ -20,7 +21,7 @@ setup(
     python_requires='>=3.6',
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
         'Operating System :: OS Independent',
     ],
     include_package_data=False,  # To include non-code files specified in MANIFEST.in
