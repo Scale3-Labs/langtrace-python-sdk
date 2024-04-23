3dd949292aa6a3a3e6bd3d388c7ea79148346f18
Karthik Kalyanaraman <karthik@scale3labs.com>
2024-04-23 16:21:03 -0700

    cohere embed

---
+++ src/examples/cohere_example/embed.py
@@ -0,0 +1,23 @@
+import cohere
+from dotenv import find_dotenv, load_dotenv
+
+from langtrace_python_sdk import langtrace
+
+# from langtrace_python_sdk.utils.with_root_span import with_langtrace_root_span
+
+_ = load_dotenv(find_dotenv())
+
+langtrace.init(write_to_langtrace_cloud=False)
+
+co = cohere.Client()
+
+
+# @with_langtrace_root_span("embed_create")
+def embed():
+    response = co.embed(
+        texts=["hello", "goodbye"],
+        model="embed-english-v3.0",
+        input_type="classification",
+    )
+    print(response)
+    return response
