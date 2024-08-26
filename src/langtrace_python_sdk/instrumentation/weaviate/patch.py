1fa3f8579b8c21df67fab3cfb30c9559662521e4
darshit-s3 <119623510+darshit-s3@users.noreply.github.com>
2024-08-26 13:34:36 +0530

    fix: weaviate create collection tracing (#298)

--- src/langtrace_python_sdk/instrumentation/weaviate/patch.py
+++ src/langtrace_python_sdk/instrumentation/weaviate/patch.py
@@ -16,21 +16,21 @@ limitations under the License.
 
 import json
 
+from importlib_metadata import version as v
 from langtrace.trace_attributes import DatabaseSpanAttributes
-from langtrace_python_sdk.utils.llm import get_span_name
 from opentelemetry import baggage, trace
 from opentelemetry.trace import SpanKind
-from opentelemetry.trace.status import Status, StatusCode
 from opentelemetry.trace.propagation import set_span_in_context
+from opentelemetry.trace.status import Status, StatusCode
+
+from langtrace_python_sdk.constants import LANGTRACE_SDK_NAME
 from langtrace_python_sdk.constants.instrumentation.common import (
     LANGTRACE_ADDITIONAL_SPAN_ATTRIBUTES_KEY,
     SERVICE_PROVIDERS,
 )
 from langtrace_python_sdk.constants.instrumentation.weaviate import APIS
+from langtrace_python_sdk.utils.llm import get_span_name
 from langtrace_python_sdk.utils.misc import extract_input_params, to_iso_format
-from importlib_metadata import version as v
-
-from langtrace_python_sdk.constants import LANGTRACE_SDK_NAME
 
 # Predefined metadata response attributes
 METADATA_ATTRIBUTES = [
@@ -43,6 +43,22 @@ METADATA_ATTRIBUTES = [
     "is_consistent",
     "rerank_score",
 ]
+
+
+def extract_inputs(args, kwargs):
+    extracted_params = {}
+    kwargs_without_properties = {k: v for k, v in kwargs.items() if k != "properties"}
+    extracted_params.update(extract_input_params(args, kwargs_without_properties))
+
+    if kwargs.get("properties", None):
+        extracted_params["properties"] = []
+        for each_prop in kwargs.get("properties"):
+            if hasattr(each_prop, "_to_dict"):
+                # append properties to extracted_params
+                extracted_params["properties"].append(each_prop._to_dict())
+
+        extracted_params["properties"] = json.dumps(extracted_params["properties"])
+    return extracted_params
 
 
 def extract_metadata(metadata):
@@ -126,7 +142,7 @@ def create_traced_method(method_name, version, tracer, get_collection_name=None)
             "db.system": "weaviate",
             "db.operation": api["OPERATION"],
             "db.collection.name": collection_name,
-            "db.query": json.dumps(extract_input_params(args, kwargs)),
+            "db.query": json.dumps(extract_inputs(args, kwargs)),
             **(extra_attributes if extra_attributes is not None else {}),
         }
 
@@ -143,7 +159,6 @@ def create_traced_method(method_name, version, tracer, get_collection_name=None)
             try:
                 # Attempt to call the original method
                 result = wrapped(*args, **kwargs)
-                print(result)
                 if api["OPERATION"] in ["query", "generate"]:
                     span.add_event(
                         name="db.response",
