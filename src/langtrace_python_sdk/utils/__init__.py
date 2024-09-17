9b6403f282c75628a996f07cc6463b72963bfbbd
Ali Waleed <ali@scale3labs.com>
2024-09-17 13:08:15 +0300

    fixing disable instrumentation logic

--- src/langtrace_python_sdk/utils/__init__.py
+++ src/langtrace_python_sdk/utils/__init__.py
@@ -1,4 +1,4 @@
-from langtrace_python_sdk.types import NOT_GIVEN
+from langtrace_python_sdk.types import NOT_GIVEN, InstrumentationType
 from .sdk_version_checker import SDKVersionChecker
 from opentelemetry.trace import Span
 from langtrace.trace_attributes import SpanAttributes
@@ -49,3 +49,44 @@ def check_if_sdk_is_outdated():
 
 def get_sdk_version():
     return SDKVersionChecker().get_sdk_version()
+
+
+def validate_instrumentations(disable_instrumentations):
+    if disable_instrumentations is not None:
+        if (
+            disable_instrumentations.get("all_except") is not None
+            and disable_instrumentations.get("only") is not None
+        ):
+            raise ValueError(
+                "Cannot specify both only and all_except in disable_instrumentations"
+            )
+
+        for key, value in disable_instrumentations.items():
+            if isinstance(value, str):
+                # Convert single string to list of enum values
+                disable_instrumentations[key] = [InstrumentationType.from_string(value)]
+            elif isinstance(value, list):
+                # Convert list of strings to list of enum values
+                disable_instrumentations[key] = [
+                    (
+                        InstrumentationType.from_string(item)
+                        if isinstance(item, str)
+                        else item
+                    )
+                    for item in value
+                ]
+            # Validate all items are of enum type
+            if not all(
+                isinstance(item, InstrumentationType)
+                for item in disable_instrumentations[key]
+            ):
+                raise TypeError(
+                    f"All items in {key} must be of type InstrumentationType"
+                )
+
+
+def is_package_installed(package_name):
+    import pkg_resources
+
+    installed_packages = {p.key for p in pkg_resources.working_set}
+    return package_name in installed_packages
