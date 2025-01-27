b13087ccacd2d906861c52f492b0228a435781cb
Karthik Kalyanaraman <karthik@scale3labs.com>
2025-01-27 16:03:32 +0530

    serper support

--- src/langtrace_python_sdk/instrumentation/crewai_tools/instrumentation.py
+++ src/langtrace_python_sdk/instrumentation/crewai_tools/instrumentation.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2024 Scale3 Labs
+Copyright (c) 2025 Scale3 Labs
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
