0d5fa7d200562ecd47f7b39ad525bf9bcc3ebf38
obinnascale3 <109410793+obinnascale3@users.noreply.github.com>
2025-04-23 17:48:37 +0300

    Obinna/s3 en 3033 add anthropic streaming (#531)
    * add instrumentation for anthropic's messages.stream method
    
    * bump version
    
    ---------
    
    Co-authored-by: Obinna Okafor <obinna.okafor01@gmail.com>

--- src/langtrace_python_sdk/constants/instrumentation/anthropic.py
+++ src/langtrace_python_sdk/constants/instrumentation/anthropic.py
@@ -3,4 +3,8 @@ APIS = {
         "METHOD": "anthropic.messages.create",
         "ENDPOINT": "/v1/messages",
     },
+    "MESSAGES_STREAM": {
+        "METHOD": "anthropic.messages.stream",
+        "ENDPOINT": "/v1/messages",
+    },
 }
